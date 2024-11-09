e3da0a13840e66079775d609742c19c4d03ec69e
YUVARAJ787 <yuvarajv787@gmail.com>
2024-11-09 18:34:59 +0530

    instances added

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,23 @@ import publications from "./publications";
 import instances from "./instances";
 import Logo from "./assets/ista_transparent_logo.webp";
 import { useEffect, useState } from "preact/hooks";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import "./index.css"
 
 export function App() {
-  // Retrieve active tab from URL or default to "publications"
   const urlParams = new URLSearchParams(window.location.search);
   const initialTab = urlParams.get("tab") || "publications";
-
   const [activeTab, setActiveTab] = useState(initialTab);
-  const years = [...new Set((activeTab === "publications" ? publications : instances).map(publication => (new Date(publication.date)).getFullYear()))].sort().reverse();
+
+  const years = [
+    ...new Set(
+      (activeTab === "publications" ? publications : instances).map(
+        (publication) => new Date(publication.date).getFullYear()
+      )
+    ),
+  ]
+    .sort()
+    .reverse();
 
   // Update URL whenever the active tab changes
   useEffect(() => {
@@ -19,52 +27,81 @@ export function App() {
     window.history.replaceState({}, "", `${window.location.pathname}?${params}`);
   }, [activeTab]);
 
+  // Preload images for both tabs once on initial render
+  useEffect(() => {
+    const images = [...publications, ...instances].map((publication) => publication.cover);
+    images.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }, []);
+
   return (
     <>
-      {localStorage.getItem('notFirstTime') !== '1' ? <Intro /> : null}
+      <Intro />
       <Header />
       <Analytics />
-      <div class="font-Azonix container mx-auto p-4">
-        <div class="flex justify-center space-x-4">
+      <div class="dark:text-white container mx-auto p-6">
+        <div class="flex justify-center font-Azonix space-x-6 mb-8">
           <button
             onClick={() => setActiveTab("publications")}
-            class={`px-4 py-2 font-bold ${activeTab === "publications" ? "bg-gray-700 text-white" : "bg-gray-300 text-black"} rounded`}
+            class={`px-6 py-3 font-bold text-lg ${
+              activeTab === "publications"
+                ? "text-white shadow-lg bg-gray-800"
+                : "text-gray-500"
+            } rounded-xl transition-colors duration-300`}
           >
-            Publications
+            Cache
           </button>
           <button
             onClick={() => setActiveTab("instances")}
-            class={`px-4 py-2 font-bold ${activeTab === "instances" ? "bg-gray-700 text-white" : "bg-gray-300 text-black"} rounded`}
+            class={`px-6 py-3 font-bold text-lg ${
+              activeTab === "instances"
+                ? "text-white shadow-lg bg-gray-800"
+                : "text-gray-500"
+            } rounded-xl transition-colors duration-300`}
           >
-            Instances
+            InsTaNces
           </button>
         </div>
 
-        <div class="mt-4">
-          {years.map(year => (
-            <div key={year} class="p-2">
-              <p class="m-2 underline text-xl">{year}</p>
+        <div class="mt-8">
+          {years.map((year) => (
+            <div key={year} class="p-8 mb-6">
+              <p class="m-2 text-3xl font-semibold dark:text-black font-Azonix">{year}</p>
               <div class="flex flex-row flex-wrap">
-                {(activeTab === "publications"
-                  ? publications
-                  : instances)
-                  .filter(publication => (new Date(publication.date).getFullYear() === year))
-                  .sort((a, b) => (new Date(b.date)).getMonth() - (new Date(a.date)).getMonth())
-                  .map(publication => {
-                    console.log(publication);
-                    return (
-                      <div key={publication.name} class="max-w-sm rounded-lg shadow-2xl m-2 relative">
-                         {/* @ts-ignore */}
-                        {publication?.highlight && <p class="animate-pulse absolute inline-flex items-center justify-center text-xs font-bold text-white bg-red-500 rounded z-10 -top-2 p-1 -end-4">{publication.highlight}</p>}
-                         {/* @ts-ignore */}
-                        <div class="_df_thumb df-book-cover m-0 w-100 img" id="flipbook_example" resource={publication.file} thumb={publication.cover}></div>
-                        <div class="p-2">
-                          <p class="font-sans font-bold text-center">{publication.name}</p>
-                          <p class="font-sans text-xs">Published on {(new Date(publication.date)).toLocaleDateString()}</p>
-                        </div>
+                {(activeTab === "publications" ? publications : instances)
+                  .filter(
+                    (publication) =>
+                      new Date(publication.date).getFullYear() === year
+                  )
+                  .sort(
+                    (a, b) =>
+                      new Date(b.date).getMonth() - new Date(a.date).getMonth()
+                  )
+                  .map((publication) => (
+                    <div
+                      key={publication.name}
+                      class="max-w-sm rounded-1xl text-black relative shadow-2xl m-4 bg-white bg-opacity-80 hover:bg-opacity-90 transition-all duration-300"
+                    >
+                      {/* @ts-ignore */}
+                      {publication?.highlight && (
+                        <p class="animate-pulse inline-flex absolute items-center justify-center text-xs font-bold text-white bg-red-500 rounded-full z-10 -top-4 right-0 px-3 py-1">
+                          {publication.highlight}
+                        </p>
+                      )}
+                      {/* @ts-ignore */}
+                      <div class="_df_thumb df-book-cover w-full h-56 rounded-t-1xl bg-cover bg-center" style={{ backgroundImage: `url(${publication.cover})` }} thumb={publication.cover} source={publication.file}></div>
+                      <div class="p-4 desc-text">
+                        <p class="desc-text font-bold text-2xl text-center tracking-tight">
+                          {publication.name}
+                        </p>
+                        <p class="desc-text text-sm text-center text-gray-600 desc-text">
+                          Published on {new Date(publication.date).toLocaleDateString()}
+                        </p>
                       </div>
-                    );
-                  })}
+                    </div>
+                  ))}
               </div>
             </div>
           ))}
@@ -79,22 +116,23 @@ function Header() {
   return (
     <nav class="p-2 bg-white border-gray-300 dark:bg-gray-900 border-b-2">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <span class="self-center text-2xl font-Azonix font-semibold whitespace-nowrap dark:text-white">
+        <a href="/" class="flex items-center space-x-2 rtl:space-x-reverse">
+          {/* Responsive font sizes */}
+          <span class="self-center text-xl md:text-2xl font-Azonix font-semibold whitespace-nowrap dark:text-white">
             Cache
           </span>
-          <span class="m-2 dark:text-white">x</span>
-          <span class="self-center font-bold font-noto text-3xl whitespace-nowrap dark:text-white instance-brand">
+          <span class="m-1 md:m-2 dark:text-white">x</span>
+          <span class="self-center font-bold font-noto text-2xl md:text-3xl whitespace-nowrap dark:text-white instance-brand">
             InsTaƞce
           </span>
         </a>
         <a
-          class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"
+          class="flex md:order-2 space-x-2 md:space-x-0 rtl:space-x-reverse"
           href="http://istaceg.in/"
           target="_blank"
         >
-          <div class="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 hover:cursor-pointer font-medium rounded-lg text-sm px-4 py-2 text-center bg-gray-500 dark:bg-gray-700">
-            <img src={Logo} class="h-10" alt="Cache Logo" />
+          <div class="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 hover:cursor-pointer font-medium rounded-lg text-sm px-3 py-1 md:px-4 md:py-2 text-center bg-gray-500 dark:bg-gray-700">
+            <img src={Logo} class="h-8 md:h-10" alt="Cache Logo" />
           </div>
         </a>
       </div>
@@ -102,9 +140,10 @@ function Header() {
   );
 }
 
+
 function Footer() {
   return (
-    <footer class="bg-white shadow dark:bg-gray-800" style={{position:"absolute", bottom: 0}}>
+    <footer class="bg-white shadow dark:bg-gray-800">
       <div class="w-full mx-auto max-w-screen-xl p-4 flex justify-center">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024
@@ -118,16 +157,31 @@ function Footer() {
   );
 }
 
+const setCookie = (name, value, minutes) => {
+  const expires = new Date(Date.now() + minutes * 60 * 1000).toUTCString();
+  document.cookie = `${name}=${value}; expires=${expires}; path=/`;
+};
+
+const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+  return null;
+};
+
+
 function Intro() {
   const [visible, setVisible] = useState(true);
-
   useEffect(() => {
-    setTimeout(() => {
+    if (getCookie("notFirstTime") !== "1") {
+      setTimeout(() => {
+        setVisible(false);
+        setCookie("notFirstTime", "1", 3); // Set cookie for 3 minutes on first view
+      }, 4500);
+    } else {
       setVisible(false);
-      localStorage.setItem("notFirstTime", "1");
-    }, 4500);
+    }
   }, []);
-
   return (
     <>
       {visible && (
@@ -138,7 +192,21 @@ function Intro() {
               return (
                 <span
                   aria-hidden="true"
-                  class="moving text-2xl md:text-4xl lg:text-5xl inline-block"
+                  class="moving text-2xl md:text-4xl lg:text-5xl inline-block font-Azonix"
+                  key={index}
+                  style={style}
+                >
+                  {char}
+                </span>
+              );
+            })}
+            <span class="m-5 text-3xl">x</span>
+            {"InsTaƞce".split("").map((char, index) => {
+              const style = { animationDelay: `${0.5 + index * 0.4}s`, fontWeight: "bolder" };
+              return (
+                <span
+                  aria-hidden="true"
+                  class="moving text-2xl md:text-4xl lg:text-5xl inline-block font-noto instance-brand"
                   key={index}
                   style={style}
                 >
--- src/instances.ts
+++ src/instances.ts
@@ -4,6 +4,7 @@ export default [
       "date": "2024-10-01T00:00:00.000+05:30",
       "file": "/instances/instance_oct_2024.pdf",
       "cover": "/instances/instance_oct_2024.webp",
+      "highlight":"Just Now"
     }
   ]
   
