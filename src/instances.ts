9dee3b5f67f43ab1fd5d212694ebca13630f76bc
ISTACEG <100031815+ISTACEG@users.noreply.github.com>
2025-02-17 09:54:11 +0530

    Added Instance of Jan 2025

--- src/instances.ts
+++ src/instances.ts
@@ -1,10 +1,16 @@
 export default [
+    {
+      "name": "Instance October 2024",
+      "date": "2024-10-01T00:00:00.000+05:30",
+      "file": "/instances/instance_jan_2025.pdf",
+      "cover": "/instances/instance_jan_2025.png",
+      "highlight":"Latest !"
+    },
   {
     "name": "Instance November 2024",
     "date": "2024-12-02T00:00:00.000+05:30",
     "file": "/instances/instance_nov_2024.pdf",
     "cover": "/instances/instance_nov_2024.png",
-    "highlight":"Just Now"
   },
     {
       "name": "Instance October 2024",
