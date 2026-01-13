d74fa45915586c489420680bc7b92fbe5d5c6cc5
Sri Dharani R <sridharanirajendran26@gmail.com>
2026-01-13 15:15:51 +0530

    october 2025 instances added

--- src/instances.ts
+++ src/instances.ts
@@ -1,31 +1,33 @@
 export default [
-  
   {
-    "name": "Instance Febrary 2025",
-    "date": "2025-03-12T00:00:00.000+05:30",
-    "file": "/instances/instance_feb_2025.pdf",
-    "cover": "/instances/instance_feb_2025.png"
-    
+    name: "Instance October 2025",
+    date: "2025-11-01T00:00:00.000+05:30",
+    file: "/instances/instance_oct_2025.pdf",
+    cover: "/instances/instance_oct_2025.webp",
+    highlight: "Fresh!",
   },
-    {
-      "name": "Instance January 2025",
-      "date": "2025-02-17T00:00:00.000+05:30",
-      "file": "/instances/instance_jan_2025.pdf",
-      "cover": "/instances/instance_jan_2025.png"
-    },
   {
-    "name": "Instance November 2024",
-    "date": "2024-12-02T00:00:00.000+05:30",
-    "file": "/instances/instance_nov_2024.pdf",
-    "cover": "/instances/instance_nov_2024.png",
+    name: "Instance Febrary 2025",
+    date: "2025-03-12T00:00:00.000+05:30",
+    file: "/instances/instance_feb_2025.pdf",
+    cover: "/instances/instance_feb_2025.png",
   },
-    {
-      "name": "Instance October 2024",
-      "date": "2024-10-01T00:00:00.000+05:30",
-      "file": "/instances/instance_oct_2024.pdf",
-      "cover": "/instances/instance_oct_2024.webp",
-    }
-  
-  
-  ]
-  
+  {
+    name: "Instance January 2025",
+    date: "2025-02-17T00:00:00.000+05:30",
+    file: "/instances/instance_jan_2025.pdf",
+    cover: "/instances/instance_jan_2025.png",
+  },
+  {
+    name: "Instance November 2024",
+    date: "2024-12-02T00:00:00.000+05:30",
+    file: "/instances/instance_nov_2024.pdf",
+    cover: "/instances/instance_nov_2024.png",
+  },
+  {
+    name: "Instance October 2024",
+    date: "2024-10-01T00:00:00.000+05:30",
+    file: "/instances/instance_oct_2024.pdf",
+    cover: "/instances/instance_oct_2024.webp",
+  },
+];
